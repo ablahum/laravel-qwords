d3e370a4dd6c2fa2f4a2322fa2023e419e43a6f8
ablahum <mriztama@gmail.com>
2023-06-04 21:37:41 +0700

    refacto

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('', function () {
-//     return view('user.index');
-// });
-
 Route::get('', function () {
     $hosting_datas = [
         [
